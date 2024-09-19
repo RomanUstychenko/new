aff6fda7ca3b17b1b1e6f313f4fbd3860ae52b31
Roman <roman.ustychenko@gmail.com>
2024-09-19 14:31:11 +0300

    Create Theme.tsx

---
+++ frontend/src/Theme.tsx
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+      mainTextLive: '#a19797',
+    },
+    fonts: {
+      mainLive: 'Rye, cursive',
+    },
+    size: {
+      navbarHeightMobile: '40px',
+    },
+  };
